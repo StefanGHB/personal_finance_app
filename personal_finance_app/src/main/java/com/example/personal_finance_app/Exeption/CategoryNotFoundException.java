a6f86e2b281786d67a3350fd436816cba71ac583
stefan <you@example.com>
2025-06-12 11:59:24 +0300

    implement CategoryNotFoundException class

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Exeption/CategoryNotFoundException.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Exeption/CategoryNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.personal_finance_app.Exeption;public class CategoryNotFoundExeption {
+package com.example.personal_finance_app.Exeption;
+
+public class CategoryNotFoundException extends RuntimeException {
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
 }
