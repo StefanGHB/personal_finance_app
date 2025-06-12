a2d8be4d95b0cce3e7cced79b230046e65a211c5
stefan <you@example.com>
2025-06-12 12:02:38 +0300

    implement BudgetRepository interface

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Repository/BudgetRepository.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Repository/BudgetRepository.java
@@ -1,2 +1,24 @@
-package com.example.personal_finance_app.Repository;public class BudgetRepository {
+package com.example.personal_finance_app.Repository;
+
+
+import com.example.personal_finance_app.Entity.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BudgetRepository extends JpaRepository<Budget, Long> {
+
+    List<Budget> findByUserId(Long userId);
+
+    List<Budget> findByUserIdAndBudgetYearAndBudgetMonth(Long userId, Integer year, Integer month);
+
+    Optional<Budget> findByUserIdAndCategoryIdAndBudgetYearAndBudgetMonth(
+            Long userId, Long categoryId, Integer year, Integer month);
+
+    // За общия месечен бюджет (category_id е NULL)
+    Optional<Budget> findByUserIdAndCategoryIsNullAndBudgetYearAndBudgetMonth(
+            Long userId, Integer year, Integer month);
 }
