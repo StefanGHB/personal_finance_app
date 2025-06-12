ed23179de6de4d45ce8ed08f8268426fe318f51c
stefan <you@example.com>
2025-06-12 12:02:03 +0300

    implement CategoryRepository interface

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Repository/CategoryRepository.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Repository/CategoryRepository.java
@@ -1,2 +1,18 @@
-package com.example.personal_finance_app.Repository;public class CategoryRepository {
+package com.example.personal_finance_app.Repository;
+
+import com.example.personal_finance_app.Entity.Category;
+import com.example.personal_finance_app.Enum.TransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findByUserId(Long userId);
+
+    List<Category> findByUserIdAndType(Long userId, TransactionType type);
+
+    List<Category> findByUserIdOrderByName(Long userId);
 }
