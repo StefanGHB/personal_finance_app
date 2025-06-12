5af7de733ebc0e3eef1b09b12c615bf9b4c3b1d7
stefan <you@example.com>
2025-06-12 12:00:56 +0300

    implement UserRepository interace

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Repository/UserRepository.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Repository/UserRepository.java
@@ -1,2 +1,19 @@
-package com.example.personal_finance_app.Repository;public class UserRepository {
+package com.example.personal_finance_app.Repository;
+
+
+import com.example.personal_finance_app.Entity.User;
+import com.example.personal_finance_app.Enum.AuthProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
+
+    boolean existsByEmail(String email);
 }
