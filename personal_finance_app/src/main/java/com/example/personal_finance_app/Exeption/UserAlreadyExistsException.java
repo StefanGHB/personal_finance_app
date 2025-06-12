5734ef50d35b2262c26f6cb4d9ae9b84262f781b
stefan <you@example.com>
2025-06-12 11:56:41 +0300

    implement UserAlreadyExistsException class

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Exeption/UserAlreadyExistsException.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Exeption/UserAlreadyExistsException.java
@@ -1,2 +1,7 @@
-package com.example.personal_finance_app.Exeption;public class UserAlreadyExistsExeprion {
+package com.example.personal_finance_app.Exeption;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
 }
