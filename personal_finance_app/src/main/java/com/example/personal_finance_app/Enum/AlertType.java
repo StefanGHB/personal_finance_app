a73c825a3f8365eb8a5ff8a142800d31d490a274
stefan <you@example.com>
2025-06-12 11:54:03 +0300

    implement AlertType enum class

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Enum/AlertType.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Enum/AlertType.java
@@ -1,2 +1,6 @@
-package com.example.personal_finance_app.Enum;public enum AlertType {
+package com.example.personal_finance_app.Enum;
+
+public enum AlertType {
+    WARNING,    // Предупреждение при приближаване към лимита (напр. 90%)
+    EXCEEDED    // Превишен бюджет
 }
