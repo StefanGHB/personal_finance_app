3ae74aaebce4c116c61df176c0919222a891ae34
stefan <you@example.com>
2025-06-13 21:33:45 +0300

    implement PasswordConfig class

--- personal_finance_app/src/main/java/com/example/personal_finance_app/Config/PasswordConfig.java
+++ personal_finance_app/src/main/java/com/example/personal_finance_app/Config/PasswordConfig.java
@@ -1,2 +1,19 @@
-package com.example.personal_finance_app.Config;public class PasswordConfig {
+package com.example.personal_finance_app.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Password Configuration
+ * Separate configuration to avoid circular dependencies
+ */
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
